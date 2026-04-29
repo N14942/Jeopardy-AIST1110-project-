58ffc3c096011741cd5428a262fa7d69cf0f1352
Yuri Lee <echristine@ewha.ac.kr>
2026-04-30 00:01:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from api_manager import QuestionManager
 def main():
     # Reset
     qm = QuestionManager()
-    game_logic = JeopardyGame(qm)
-    ui = Interface()
+    game_logic = JeopardyGame()
+    ui = Interface(game_logic)
 
     # Run Main Loop
     ui.run() # Run Function 'run()' in 'interface.py'
+
 
 if __name__ == "__main__":
     main()
